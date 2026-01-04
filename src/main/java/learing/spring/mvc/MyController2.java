48365253c37d568ee2fcbf9891f3e0d9c1b4a0d9
Mani Kant Kumar <manikantraj060@gmail.com>
2026-01-04 13:30:54 +0530

    update

---
+++ src/main/java/learing/spring/mvc/MyController2.java
@@ -0,0 +1,32 @@
+package learing.spring.mvc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController2 {
+	
+	@GetMapping
+	public String greetings()
+	{
+		System.out.println("MyController2.greetings()");
+		return "welcome";
+	}
+	
+
+	
+	
+	@RequestMapping(value = {"/admin"})
+	public String admin(@RequestParam(name ="username",defaultValue = "Guest")String userName,Model model)
+	{
+		System.out.println("MyController.admin : "+userName);
+		
+		model.addAttribute("userName", userName);
+		
+		return "admin";
+	}
+}
---
+++ src/main/webapp/WEB-INF/JSP/admin.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Web-MVC</title>
+</head>
+<body bgcolor="pink">
+	<h1>This is admin portal</h1>
+	<h2>UserName : ${userName} </h2>
+</body>
+</html>
